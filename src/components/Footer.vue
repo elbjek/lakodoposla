5b56b6ba027bc2197c5b19b1295eb200f24ab7f7
elbjek <lenabjekovic@hotmail.com>
2021-04-27 21:53:30 +0200

    Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,85 @@
 <template>
   <footer class="footer">
-    footer
+    <div class="container">
+      <div class="footer-top">
+        <div class="row">
+          <div class="col-sm-2">Logo</div>
+          <div class="col-sm-4 text-bold">Lako do posla - Berza poslova</div>
+        </div>
+        <div class="row">
+          <div class="col-sm-12 col-lg-2">
+            <p>© 2021 Sva prava zadržana</p>
+          </div>
+          <div class="col-sm-12 col-lg-2">
+            <ul>
+              <li>
+                <a href="/">Posao Beograd</a>
+              </li>
+              <li>
+                <a href="/">Posao Novi Sad</a>
+              </li>
+              <li>
+                <a href="/">Posao Nis</a>
+              </li>
+              <li>
+                <a href="/">Posao Kragujevac</a>
+              </li>
+              <li>
+                <a href="/">Posao Inostranstvo</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-sm-12 col-lg-2">
+            <ul>
+              <li><a href="/">O nama</a></li>
+              <li><a href="/">Kontakt</a></li>
+              <li><a href="/">Pitanja i odgovori</a></li>
+              <li><a href="/">Saveti</a></li>
+              <li><a href="/">Uslovi koriscenja</a></li>
+              <li><a href="/">Politika privatnosti</a></li>
+            </ul>
+          </div>
+          <div class="col-sm-12 col-lg-2">
+            <ul>
+              <li><a href="/">O nama</a></li>
+              <li><a href="/">Kontakt</a></li>
+              <li><a href="/">Pitanja i odgovori</a></li>
+              <li><a href="/">Saveti</a></li>
+              <li><a href="/">Uslovi koriscenja</a></li>
+              <li><a href="/">Politika privatnosti</a></li>
+            </ul>
+          </div>
+          <div class="col-sm-12 col-lg-2">
+            <ul>
+              <li><a href="/">Poslodavci</a></li>
+              <li><a href="/">Poslodavci - Cenovnik usluga</a></li>
+              <li><a href="/">Medijski oglas za posao</a></li>
+              <li><a href="/">Vruc oglas za posao</a></li>
+              <li><a href="/">Prioritetni oglas za posao</a></li>
+              <li><a href="/">Obicni oglas za posao</a></li>
+            </ul>
+          </div>
+          <div class="col-sm-12 col-lg-2">
+            <p class="text-bold">U saradnji sa Talentmarket</p>
+            <img src="../assets/talentmarket.png" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer-bottom">
+        <ul>
+            <li><a href="/">Lako do posla</a>&nbsp;|&nbsp;</li>
+            <li><a href="/">Lako do znanja</a>&nbsp;|&nbsp; </li>
+            <li><a href="/">Lako do kola</a></li>
+        </ul>
+    </div>
   </footer>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Footer',
 
-  data: () => ({
-  }),
+  data: () => ({}),
 };
 </script>
